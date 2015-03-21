0b684bf9d728bdcabdbd15a5fb132cfd2f5b8132
Leonic255 <leonic255@gmail.com>
2015-03-21 18:12:35 +0800

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.smtp_settings = (
+  config.action_mailer.smtp_settings =(
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
@@ -34,14 +34,13 @@ Rails.application.configure do
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
-    )
-
-  # ActionMAiler Config
-  config.action_mailer.default_url_options = ( :host => 'localhost3000')
+  )
+  # ActionMailer Config
+  config.action_mailer.default_url_options = ( :host => 'localhost:3000' )
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 
-  #Send emaial in development mode?
+  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
