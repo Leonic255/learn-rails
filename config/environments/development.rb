047406c31933b7e149289eda4cc21eeed89c400e
Leonic255 <leonic255@gmail.com>
2015-03-21 15:15:24 +0800

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.domain_name
-    password: Rails.application.secrets.domain_password
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
     )
 
   # ActionMAiler Config
